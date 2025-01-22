f8cdab9a24975d2416a1f3428a34b99ba141564d
Rafael-Armond <rafaelarmond054@gmail.com>
2025-01-22 01:05:53 -0300

    dale

--- app/useCases/shipment/create_shipment_use_case.py
+++ app/useCases/shipment/create_shipment_use_case.py
@@ -3,6 +3,6 @@ from app.main import socketio
 
 class CreateShipmentUseCase:
     def execute(self, data):
-        print("Teste 123: ", data)
+        print("Enviando remessas ", data)
         socketio.emit("new_shipment", data, to=None)
         return "Remessa enviada ao cliente"
